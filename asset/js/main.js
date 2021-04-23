56de627ef680ca51985e8f06d81ef4a718415de4
valeriomatranga <matrangav@gmail.com>
2021-04-23 17:31:11 +0200

    calcolo sconto in base eta

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,16 +1,13 @@
 //distanza da percorrere 
 var distanza = prompt("Quanti km devi percorrere?")
-console.log(distanza);
+
+if ( distanza <1 || isNaN(distanza)){
+    alert("Devi mettere il numero dei km da percorrere")
+}
 
 //eta del passeggero
 var eta = prompt("Quanti anni hai?")
 console.log(eta);
-
-//eta minore
-var minore = ( <= 17);
-
-//pensionato
-var pensionato = ( >= 65);
 
 //costo del biblietto (0.21€ al km)
 var km = distanza * 0.21;
@@ -22,10 +19,14 @@ var sconto20 = (km * 20) / 100;
 //sconto del 40% per over 65
 var sconto40 = (km * 40) / 100;
 
-//costo del biglietto minori 18 (0.21€ al km - 20%)
-var prezzoMinori = km - sconto20;
-console.log(prezzoMinori);
-
-//costo del biglietto over 65 (0.21€ al km - 40%)
-var prezzoPensionati = km - sconto40;
-console.log(prezzoPensionati)
+if ( eta <1 || eta >100 || isNaN(eta)){
+    alert("devi inserire la tua eta!")
+}
+if(eta < 18){
+   sconto20
+   console.log(km - sconto20);
+}
+if(eta > 65){
+    sconto40
+    console.log(km -sconto40);
+}
