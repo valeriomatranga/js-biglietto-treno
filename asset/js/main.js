0a90872c19e37c68735e96007d41c967956c1c2c
valeriomatranga <matrangav@gmail.com>
2021-04-23 18:26:10 +0200

    correzzione con inserimento innerhtml

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,32 +1,42 @@
 //distanza da percorrere 
-var distanza = prompt("Quanti km devi percorrere?")
+var distanza = prompt("Quanti km devi percorrere?");
 
-if ( distanza <1 || isNaN(distanza)){
-    alert("Devi mettere il numero dei km da percorrere")
+if ( distanza < 1 || isNaN(distanza)){
+    alert("Devi mettere il numero dei km da percorrere");
 }
+
+
 
 //eta del passeggero
 var eta = prompt("Quanti anni hai?")
-console.log(eta);
-
-//costo del biblietto (0.21€ al km)
-var km = distanza * 0.21;
-console.log(km);
-
-//sconto del 20% per i minorenni
-var sconto20 = (km * 20) / 100;
-
-//sconto del 40% per over 65
-var sconto40 = (km * 40) / 100;
 
 if ( eta <1 || eta >100 || isNaN(eta)){
     alert("devi inserire la tua eta!")
 }
+
+
+
+//costo del biblietto (0.21€ al km)
+var prezzo = distanza * 0.21;
+
+var prezzoScontato = prezzo;
+
+
+//sconto del 20% per i minorenni
+var sconto20 = (prezzo * 20) / 100;
+
+//sconto del 40% per over 65
+var sconto40 = (prezzo * 40) / 100;
+
 if(eta < 18){
-   sconto20
-   console.log(km - sconto20);
+    prezzoScontato = prezzo - sconto20;
+    console.log(prezzo - sconto20);
 }
 if(eta > 65){
-    sconto40
-    console.log(km -sconto40);
+    prezzoScontato = prezzo - sconto40;
+    console.log(prezzo - sconto40);
 }
+
+prezzoScontato = prezzoScontato.toFixed(2);
+
+document.getElementById("costo").innerHTML ="Il prezzo del tuo biglietto è: € " + prezzoScontato;
